bcdc81ea7ce7f19e83dacf92f6c2bcc3db4746bd
Brice Sanchez <brice@bisscomm.com>
2014-09-23 13:19:53 -0400

    Update engine syntax (temp disable of Dragonfly to speed up debugging)

--- lib/refinery/jobs.rb
+++ lib/refinery/jobs.rb
@@ -13,8 +13,19 @@ module Refinery
     autoload :Validators, 'refinery/jobs/validators'
 
     class << self
+      attr_writer :root
+      attr_writer :tabs
+
       def root
         @root ||= Pathname.new(File.expand_path('../../../', __FILE__))
+      end
+
+      def tabs
+        @tabs ||= []
+      end
+
+      def version
+        ::Refinery::Jobs::Version.to_s
       end
 
       def factory_paths
--- lib/refinery/jobs/engine.rb
+++ lib/refinery/jobs/engine.rb
@@ -4,20 +4,19 @@ module Refinery
       include Refinery::Engine
 
       isolate_namespace Refinery::Jobs
-      engine_name :refinery_jobs
+      # engine_name :refinery_jobs
 
       config.autoload_paths += %W( #{config.root}/lib )
 
-      initializer 'attach-jobs-resumes-via-refinery-resources-with-dragonfly', :after => 'attach-refinery-resources-with-dragonfly' do |app|
-        ::Refinery::Jobs::Dragonfly.configure!
-        ::Refinery::Jobs::Dragonfly.attach!(app)
-      end
+      # initializer 'attach-jobs-resumes-via-refinery-resources-with-dragonfly', :after => 'attach-refinery-resources-with-dragonfly' do |app|
+      #   ::Refinery::Jobs::Dragonfly.configure!
+      #   ::Refinery::Jobs::Dragonfly.attach!(app)
+      # end
 
       initializer 'init plugin' do
         Refinery::Plugin.register do |plugin|
           plugin.pathname   = root
           plugin.name       = "refinery_jobs"
-          plugin.version    = '2.0.0'
           plugin.menu_match = /(admin|refinery)\/(jobs|vacancies)\/jobs$/
           plugin.activity   = { :class_name => :'refinery/jobs/job' }
           plugin.url        = proc { Refinery::Core::Engine.routes.url_helpers.jobs_admin_jobs_path }
@@ -26,7 +25,7 @@ module Refinery
       end
 
       config.after_initialize do
-        Refinery.register_engine(Refinery::Jobs)
+        Refinery.register_extension(Refinery::Resources)
       end
     end
   end

f33e11d73ca3980150fdb27482fd8db92b4d346f
Brice Sanchez <brice@bisscomm.com>
2014-09-23 13:20:36 -0400

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Refinery::Core::Engine.routes.draw do
     resources :jobs, :only => [:show, :index] do
       resources :job_applications
     end
+  end
 
-    namespace :admin, :path => 'refinery' do
+  namespace :jobs, :path => '' do
+    namespace :admin, :path => Refinery::Core.backend_route do
       resources :jobs, :except => :show do
         collection do
           post :update_positions

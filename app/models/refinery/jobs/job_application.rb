15b33efe2a0ceb61663c400e6a388730e76d68c8
Brice Sanchez <brice@bisscomm.com>
2014-09-23 17:04:34 -0400

    Update model job_application (WIP)

--- app/models/refinery/jobs/job_application.rb
+++ app/models/refinery/jobs/job_application.rb
@@ -7,7 +7,7 @@ module Refinery
 
       # ::Refinery::Jobs::Dragonfly.setup!
 
-      HUMANIZED_COLUMNS = {:resume_file_name => "Resume"}
+      # HUMANIZED_COLUMNS = {:resume_file_name => "Resume"}
 
       belongs_to :job, :class_name => "Refinery::Jobs::Job", :foreign_key => "job_id"
 
@@ -17,16 +17,16 @@ module Refinery
       validates :email, format: {
         with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
       }, length: { maximum: 255 }
-      validates             :resume, :presence => true
-      validates_with Refinery::Jobs::Validators::FileSizeValidator
+      # validates             :resume, :presence => true
+      # validates_with Refinery::Jobs::Validators::FileSizeValidator
 
-      def self.human_attribute_name(attribute, options={})
-        HUMANIZED_COLUMNS[attribute.to_sym] || super
-      end
+      # def self.human_attribute_name(attribute, options={})
+      #   HUMANIZED_COLUMNS[attribute.to_sym] || super
+      # end
 
-      def self.latest(number = 5)
-        limit(number).order('created_at DESC')
-      end
+      # def self.latest(number = 5)
+      #   limit(number).order('created_at DESC')
+      # end
     end
   end
 end

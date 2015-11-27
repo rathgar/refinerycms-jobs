c16678442714d8dd40feb35cf9deb8d768223e0f
Brice Sanchez <brice@bisscomm.com>
2015-11-27 14:06:20 -0500

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Refinery::Core::Engine.routes.draw do
-  namespace :jobs, :path => Refinery::Jobs.page_url do
-    root :to => "jobs#index"
+  namespace :jobs, path: Refinery::Jobs.page_url do
+    root to: "jobs#index"
 
-    resources :jobs, :path => '', only: [:index, :show] do
+    resources :jobs, path: '', only: [:index, :show] do
       resources :job_applications, only: [:new, :create, :show]
     end
   end
 
-  namespace :jobs, :path => '' do
-    namespace :admin, :path => Refinery::Core.backend_route do
-      resources :jobs, :except => :show do
+  namespace :jobs, path: '' do
+    namespace :admin, path: Refinery::Core.backend_route do
+      resources :jobs, except: :show do
         collection do
           post :update_positions
         end
